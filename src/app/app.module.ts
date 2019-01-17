e79e13508762a188d0eb0b6a3202ff742b08bf8b
David Maier <kennekeinennamen@gmail.com>
2019-01-17 13:21:59 +0100

    Add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
