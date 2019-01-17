329d1ad26045b9c962102c9b1e4815b2751d7841
David Maier <kennekeinennamen@gmail.com>
2019-01-17 18:08:49 +0100

    Add autocomplete searchbox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import { LocationClient, Location } from '../../restclient/restclient'
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private locationClient : LocationClient) { }
 
   ngOnInit() {
-  }
+    console.log(this.locationClient);
+    this.locationClient.getAll().subscribe(val =>{
+      this.source = val;
+    });
+    console.log(this.source);
 
+  }
+  source: Location[];
+
+  text: string;
+
+  results: Location[];
+
+
+
+  search(event) {
+    var temp: Location[] = [];
+    this.source.forEach(element => {
+      console.log(element + " " + (element.communityName.indexOf(event.query) != -1));
+      if(element.communityName.indexOf(event.query) != -1){
+        temp.push(element);
+      }
+    this.results = temp;
+    });
+  }
 }
