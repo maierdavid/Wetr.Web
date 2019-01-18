2510ad471588c438454a9bd501e6b0ea366818fb
David Maier <kennekeinennamen@gmail.com>
2019-01-18 01:58:42 +0100

    Add empty suggestion if no stations are found

--- src/app/suggestions/suggestions.component.ts
+++ src/app/suggestions/suggestions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-suggestions',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./suggestions.component.css']
 })
 export class SuggestionsComponent implements OnInit {
+
+  @Input() location: Location;
 
   constructor() { }
 
