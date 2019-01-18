701ae86aeeea9f77a0edee4dbe87df956caa89d3
David Maier <kennekeinennamen@gmail.com>
2019-01-18 11:51:18 +0100

    Comment the suggestions

--- src/app/suggestions/suggestions.component.ts
+++ src/app/suggestions/suggestions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { StationClient, LocationClient, Location, Station } from '../../restclient/restclient';
 
 @Component({
   selector: 'app-suggestions',
@@ -9,9 +10,24 @@ export class SuggestionsComponent implements OnInit {
 
   @Input() location: Location;
 
-  constructor() { }
+  locations: Location[];
+  stations: Station[] = []
+
+  constructor(private stationClient : StationClient, private locationClient : LocationClient) { }
 
   ngOnInit() {
+    // this.sameDistrict();
+  }
+
+  sameDistrict() {
+    this.locationClient.getByDistrict(this.location.districtName).subscribe(val => {
+      this.locations = val;
+      this.locations.forEach(value => {
+        this.stationClient.getByLocation(value.postCode).subscribe(station => {
+            this.stations.concat(station);
+        })
+      })
+    });
   }
 
 }
