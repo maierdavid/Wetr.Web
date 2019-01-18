4ae1f1bdfca038a135816102262aed6d0fd31069
David Maier <kennekeinennamen@gmail.com>
2019-01-18 01:05:28 +0100

    Fetch location and stations async

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { StationClient, Station, LocationClient, Location } from '../../restclient/restclient';
 
 @Component({
   selector: 'app-location',
@@ -8,12 +9,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class LocationComponent implements OnInit {
 
-  constructor(private route : ActivatedRoute) { }
+  constructor(private route : ActivatedRoute, private stationClient : StationClient, private locationClient : LocationClient) { }
 
   ngOnInit() {
     this.postCode = +this.route.snapshot.paramMap.get('postCode');
+    this.locationClient.getByCode(this.postCode).subscribe(val => {this.location = val})
+    this.stationClient.getByLocation(this.postCode).subscribe(val => {this.stations = val});
   }
 
   postCode: number;
 
+  stations: Station[];
+  location: Location;
 }
