17085c539cc0a7e3d5cf7631f0261f503b6fff80
David Maier <kennekeinennamen@gmail.com>
2019-01-19 01:50:47 +0100

    Implement simple login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import { AuthenticationService } from '../../services/authentication/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService : AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  username: string;
+  password: string;
+
+  success: boolean;
+
+  onSubmit = function () {
+    this.authenticationService.login(this.username, this.password).then(val => {
+      this.success = val;
+    });
   }
 
 }
