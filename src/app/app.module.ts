ad75b651c5b5a40e2ca8512c00bb322d0da7f289
David Maier <kennekeinennamen@gmail.com>
2019-01-19 01:16:56 +0100

    Restructure imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import { ButtonModule } from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
-import {AutoCompleteModule} from 'primeng/autocomplete';
-import { LocationClient } from '../restclient/restclient';
-import { ButtonModule } from 'primeng/button';
 import { SuggestionsComponent } from './suggestions/suggestions.component';
 import { LocationComponent } from './location/location.component';
 import { StationComponent } from './station/station.component';
@@ -36,10 +37,11 @@ import { LoginComponent } from './login/login.component';
     BrowserAnimationsModule,
     AutoCompleteModule,
     ButtonModule,
-  ],
-  providers: [
+    InputTextModule,
+    PasswordModule,
 
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
