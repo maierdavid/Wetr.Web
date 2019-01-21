dcf5ac15da469b71ce3f6b36361307710c1e4c77
David Maier <kennekeinennamen@gmail.com>
2019-01-21 05:56:01 +0100

    Add mock-implementation to the cards

--- src/app/station-report/station-report.component.ts
+++ src/app/station-report/station-report.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Station, MeasurementType } from '../../services/restclient/restclient';
+import { Station, MeasurementType, MeasurementClient, Measurement, GetMeasurementTupel } from '../../services/restclient/restclient';
+
+interface typeValue {
+  type: string;
+  value: Number;
+}
 
 @Component({
   selector: 'app-station-report',
@@ -10,15 +15,59 @@ export class StationReportComponent implements OnInit {
 
   @Input() station: Station;
   @Input() aggregation: string;
-  @Input() type: MeasurementType[];
+  @Input() types: any[];
 
-  constructor() { }
+  constructor(private measurementClient : MeasurementClient) { }
 
   ngOnInit() {
+
+    this.types.forEach(type => {this.fillData(type);});
   }
 
-  removeCard = function() {
-    
+  displayData: typeValue[] = [];
+
+  //because of JSON stringify type turns into Type
+  fillData(type: any) {
+    //unfortunately, this has to be mocked because the calls to the api yield no results
+
+    console.log(type.Type);
+
+    var generatedValue: Number;
+    switch(type.Type){
+      case("Air_Pressure"): {
+        generatedValue = this.getRandomArbitrary(650, 1020);
+        break;
+      }
+      case("Air_Temperature"): {
+        generatedValue = this.getRandomArbitrary(-20, 35);
+        break;
+      }
+      case("Humidity"): {
+        generatedValue = this.getRandomArbitrary(0, 100);
+        break;
+      }
+      case("Percipitation"): {
+        generatedValue = this.getRandomArbitrary(0, 35);
+        break;
+      }
+      case("Wind_Direction"): {
+        generatedValue = this.getRandomArbitrary(9000, 10000);
+        break;
+      }
+      case("Wind_Speed"): {
+        generatedValue = this.getRandomArbitrary(0, 100);
+        break;
+      }
+      default: {
+        generatedValue = 0;
+      }
+    }
+    this.displayData.push({type: type.Type, value: generatedValue});
+    console.log(this.displayData);
+  }
+
+  getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
   }
 
 }
